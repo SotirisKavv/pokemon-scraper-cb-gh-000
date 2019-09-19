07e252c9f3f33a230223b601abf735c16553b827
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-19 16:09:24 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+
+  attr_accessor :name, :id, :type, :db
+
+  def initialize(init_hash)
+    init_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
+  end
+
+  def self.find(id, db)
+    ret = db.execute("SELECT * FROM pokemon WHERE id = ?;", id)
+    pokemon = Pokemon.new({id:ret[0][0], name:ret[0][1], type:ret[0][2]})
+  end
 end
